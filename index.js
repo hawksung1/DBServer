3f0387d42a47aaa157caefabd494757514f9818a
hjwook <hjwook1222@naver.com>
2018-12-01 18:48:23 +0900

    hey

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 
 app.use('/', router);
-
+/*sadff*/
 app.use((req, res, next) => {
   let err = new Error('Not Found');
   err.status = 404;
