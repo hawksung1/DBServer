b2a72964f3f0306678cb962dbe2812da932ec68a
Hyun <hawksung1@gmail.com>
2018-12-03 20:52:58 +0900

    약간의 html 및 빈칸 제출 오류 수정

--- routers/request.js
+++ routers/request.js
@@ -21,18 +21,6 @@ router.get('/doing', wrapper.asyncMiddleware(async (req, res, next) => {
 		+' where MemberID = "hjwook"))'
 +' order by RID');
   res.json(request);
-/*
-  console.log('Select *'
-+'from Request'
-+'where RID IN('
-	+'Select RID'
-	+'from Attend'
-	+'where TName IN ('
-		+'Select TeamName'
-		+'from TeamMember'
-		+'where MemberID = "hjwook"))'
-+'order by RID');
-*/
 }));
 
 router.get('/orderbydate', wrapper.asyncMiddleware(async (req, res, next) => {
@@ -43,6 +31,25 @@ router.get('/orderbydate', wrapper.asyncMiddleware(async (req, res, next) => {
 router.get('/orderbypay', wrapper.asyncMiddleware(async (req, res, next) => {
   const request = await db.getQueryResult('SELECT * FROM Request where State = 0 ORDER BY Pay');
   res.json(request);
+}));
+router.get('/apply_request', wrapper.asyncMiddleware(async (req, res, next) => {
+  const request = await db.getQueryResult('Select * '
++'from Request'
++' where RID IN('
+	+' Select RID'
+	+' from Apply'
+	+' where TName IN ('
+		+' Select TeamName'
+		+' from TeamMember'
+		+' where MemberID = "hjwook"))'
++' order by RID');
+  res.json(request);
+}));
+
+router.post('/apply', wrapper.asyncMiddleware(async (req, res, next) =>{
+  const newId = req.body.id;
+  console.log(await db.getQueryResult(`select * from Request where RID = '${newId}'`));
+  res.json({success: true});
 }));
 /*
 router.post('/insert', wrapper.asyncMiddleware(async (req, res, next) =>{
