48f4ede1dce231a4a9e0318e3e324091c16152d3
hjwook <hjwook1222@naver.com>
2018-12-04 16:10:59 +0900

    board가 현재 사용자기준으로 보여지도록

--- routers/request.js
+++ routers/request.js
@@ -3,22 +3,45 @@ const express = require('express');
 const router = express.Router();
 const wrapper = require('../modules/wrapper');
 const db = require('../modules/db');
+const session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended:false}));
+router.use(session({
+  secret: 'fqiwofqewmf!@#$fqf',
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore()
+}));
 
 router.get('/', wrapper.asyncMiddleware(async (req, res, next) => {
   const request = await db.getQueryResult('SELECT * FROM Request where State = 0');
   res.json(request);
 }));
 
+router.get('/cur_log', wrapper.asyncMiddleware(async (req, res, next) => {
+  var user_id = req.session.user_id;
+  //console.log("session id = " + user_id);
+  var result = user_id;
+  res.json(result);
+  //res.send({result:result});
+}));
+
 router.get('/doing', wrapper.asyncMiddleware(async (req, res, next) => {
+  var user_id = req.session.user_id;
+  console.log("session id = " + user_id);
+  //var result = user_id;
+  //res.send({result:result});
   const request = await db.getQueryResult('Select * '
 +'from Request'
-+' where RID IN('
++' where RID IN ('
 	+' Select RID'
 	+' from Attend'
 	+' where TName IN ('
 		+' Select TeamName'
 		+' from TeamMember'
-		+' where MemberID = "hjwook"))'
+		+' where MemberID = ' +'"'+ user_id +'"'+ ' ))'
 +' order by RID');
   res.json(request);
 }));
@@ -33,23 +56,35 @@ router.get('/orderbypay', wrapper.asyncMiddleware(async (req, res, next) => {
   res.json(request);
 }));
 router.get('/apply_request', wrapper.asyncMiddleware(async (req, res, next) => {
+  var user_id = req.session.user_id;
   const request = await db.getQueryResult('Select * '
 +'from Request'
-+' where RID IN('
++' where RID IN ('
 	+' Select RID'
 	+' from Apply'
 	+' where TName IN ('
 		+' Select TeamName'
 		+' from TeamMember'
-		+' where MemberID = "hjwook"))'
+		+' where MemberID = ' +'"'+ user_id +'"'+ ' ))'
 +' order by RID');
   res.json(request);
 }));
 
 router.post('/apply', wrapper.asyncMiddleware(async (req, res, next) =>{
   const newId = req.body.id;
+  const request = await db.getQueryResult('Select count(*)'
+ + ' from SkilledAt as s, RequireLang as r'
++' where FID = "hjwook" and RID = "1" '
++ ' and s.LangName = r.LangName and s.Skill>= r.Skill');
+  /*
+  if(request = "2"){
+    console.log("비교성공");
+  }
+  */
   console.log(await db.getQueryResult(`select * from Request where RID = '${newId}'`));
-  res.json({success: true});
+  res.json({success : "Updated Successfully", status : 200});
+  //res.json("{\"msg\":\"success\"}");
+  //res.json({success: true, error: false});
 }));
 /*
 router.post('/insert', wrapper.asyncMiddleware(async (req, res, next) =>{
