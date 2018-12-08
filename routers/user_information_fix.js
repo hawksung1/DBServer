37dc9709b5801b5fc64e3888859dffd4be129d9c
Hyun <hawksung1@gmail.com>
2018-12-08 21:06:22 +0900

    download 야매로 일단 구현

--- routers/user_information_fix.js
+++ routers/user_information_fix.js
@@ -6,11 +6,48 @@ const session = require('express-session');
 const path = require('path');
 const db = require('../modules/db');
 const multer = require('multer');
+// const Axios = require('axios');
 var app = express();
 var FileStore = require('session-file-store')(session);
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var download = require('download-file');
+//move file1.htm from 'test/' to 'test/dir_1/'
+// moveFile('./test/file1.htm', './test/dir_1/');
+//moves the $file to $dir2
+var moveFile = (file, dir2)=>{
+  //include the fs, path modules
+  var fs = require('fs');
+  var path = require('path');
+
+  //gets file name and adds it to dir2
+  var f = path.basename(file);
+  var dest = path.resolve(dir2, f);
+
+  fs.rename(file, dest, (err)=>{
+    if(err) throw err;
+    else console.log('Successfully moved');
+  });
+};
+//example, copy file1.htm from 'test/dir_1/' to 'test/'
+// copyFile('./test/dir_1/file1.htm', './test/');
+//copy the $file to $dir2
+var copyFile = (file, dir2)=>{
+  //include the fs, path modules
+  var fs = require('fs');
+  var path = require('path');
+
+  //gets file name and adds it to dir2
+  var f = path.basename(file);
+  var source = fs.createReadStream(file);
+  var dest = fs.createWriteStream(path.resolve(dir2, f));
+
+  source.pipe(dest);
+  source.on('end', function() { console.log('Succesfully downloaded'); });
+  source.on('error', function(err) { console.log(err); });
+};
+
+
 
 router.use(bodyParser.urlencoded({extended:false}));
 router.use(session({
@@ -44,7 +81,6 @@ router.post('/fix_freelancer', wrapper.asyncMiddleware(async (req, res, next) =>
   if(newMajor) await db.getQueryResult(`UPDATE Freelancer SET Major='${newMajor}' WHERE FID='${user_id}'`);
   res.json({success:true});
 }));
-
 router.post('/fix_projclient', wrapper.asyncMiddleware(async (req, res, next) =>{
   const newPassword = req.body.password;
   const newName = req.body.name;
@@ -55,7 +91,6 @@ router.post('/fix_projclient', wrapper.asyncMiddleware(async (req, res, next) =>
   if(newPhone) await db.getQueryResult(`UPDATE ProjClient SET PhoneNumber='${newPhone}' WHERE PID='${user_id}'`);
   res.json({success:true});
 }));
-
 router.get('/get_my_information', wrapper.asyncMiddleware(async (req, res, next) =>{
   const user_id = req.session.user_id;
   var freelancer_sql = `SELECT * FROM Freelancer where FID = '${user_id}'`;//'${newId}'
@@ -69,26 +104,18 @@ router.get('/get_my_information', wrapper.asyncMiddleware(async (req, res, next)
   console.log(result);
   res.send({result:result});
 }));
-
 router.get('/c_download_file', wrapper.asyncMiddleware(async (req, res, next) =>{
 //working on
-
   const user_id = req.session.user_id;
   var sql = `SELECT DocName FROM OuterPortfolio WHERE FID = '${user_id}'`;
   var fileName =await db.getQueryResult(sql);
   // console.log(fileName);
   fileName = fileName[0].DocName.toString();
   // console.log(fileName);
+  var download_path = __dirname + "/download/";
   var file_path = __dirname + "/upload/" + fileName;
+  copyFile(file_path,download_path);
 
-  res.download(file_path, function(err){
-    if (err){
-      console.log(err);
-    } else {
-      console.log('downloading successful');
-    }
-  });
-  // res.send
 }));
 
 router.get('/c_show_file', wrapper.asyncMiddleware(async (req, res, next) =>{
