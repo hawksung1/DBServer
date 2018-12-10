5185449b8584c5c13ad953c580ae920b8a675804
LemonQ9409 <mnismiss@naver.com>
2018-12-10 13:55:05 +0900

    연결2차

--- routers/request_PC.js
+++ routers/request_PC.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const wrapper = require('../modules/wrapper');
 const db = require('../modules/db');
 
-router.get('/', wrapper.asyncMiddleware(async (req, res, next) => {
+router.get('/', (req, res, next) => {
+	//console.log(__dirname, '../public/html/board.html');
+	res.type('html').sendFile(path.join(__dirname, '../public/html/requestpage.html'));
+	//res.sendFile(__dirname+"/../public/html/board.html");
+});
+
+router.get('/request', wrapper.asyncMiddleware(async (req, res, next) => {
   const request = await db.getQueryResult('SELECT * FROM Request ');
   res.json(request);
 }));
@@ -28,10 +34,10 @@ router.post('/insert', wrapper.asyncMiddleware(async (req, res, next) =>{
   const newMaxNum = req.body.maxNum;
   const newCyear = req.body.cyear;
   //var newStartDate = 'SELECT NOW()';
-  
+
 
   console.log(await db.getQueryResult(`INSERT INTO Request ( RID, PAY, MinCareer, MinNum, MaxNum) values ('${newRnum}','${newPay}','${newCyear}','${newMinNum}','${newMaxNum}' )`));
-  
+
   //console.log(await db.getQueryResult(`INSERT INTO Freelancer (FID,FName,Age,PhoneNumber,Career,Major,Pwd) values ('${newId}','${newName}','${newAge}','${newPhone}','${newCareer}','${newMajor}','${newPassword}')`));
   res.json({success: true});
 }));
