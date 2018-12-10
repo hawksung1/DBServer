0e07b6cb4c7d787c7ab6743f686e4f25a1d3118c
LemonQ9409 <mnismiss@naver.com>
2018-12-10 21:11:17 +0900

    신청 팀 리스트 삭제기능 추가

--- routers/teamList.js
+++ routers/teamList.js
@@ -40,17 +40,32 @@ router.post('/getTeam', wrapper.asyncMiddleware(async (req, res, next) => {
 
 router.post('/selectTeam', wrapper.asyncMiddleware(async (req, res, next) => {
 
-  const getTeamName = req.body.teamName;
-  //const getRID = req.body.rID;
+  const getRID = req.body.rID; //Attend 에 넣을 의뢰 ID
+  const getTeamName = req.body.teamName; //Attend 에 넣을 팀이름
+
+  const getPID = req.session.user_id; //Attend 에 넘어간 의뢰를 수정하기 위해 필요한 의뢰자 ID
+
 
   //teamName -> Attend => RID(의뢰 ID), TName(팀 이름)
 
-  //console.log("의뢰 ID: "+getRID);
   console.log("팀 이름: "+getTeamName);
+  console.log("의뢰자 ID: "+getPID);
+  console.log("의뢰 ID: "+getRID);
+  //Attend table에 수락한 팀이름과 그 의뢰 삽입
+  console.log(await db.getQueryResult(`INSERT INTO Attend ( RID, TName ) values ('${getRID}','${getTeamName}')`));
 
- // console.log(await db.getQueryResult(`INSERT INTO Attend ( RID, TName ) values ('${getRID}','${getTeamName}')`));
-  //console.log(await db.getQueryResult('UPDATE  Request SET State = "1" WHERE RID = "'+getRID+'"'));
+  //Attend table에 들어갔는지 콘솔로그 확인
+  console.log(await db.getQueryResult('SELECT * FROM Attend where RID = "'+getRID+'"'));
+
+
+const curdate = await db.getQueryResult('SELECT curdate() ');
+console.log(curdate);
+  //팀을 정했으니 의뢰 상태를 구인중:0 에서 구인완료 및 진행중:1 으로 업데이트
+  console.log(await db.getQueryResult('UPDATE  Request SET StartDate=curdate(), State = "1" WHERE RID = "'+getRID+'" AND PID = "'+getPID+'" '));
   //const getTeamList = await db.getQueryResult('SELECT * FROM Attend where RID = "'+getRID+'"');
+
+  //의뢰가 구인 완료 및 진행 중이므로, 그 의뢰에 선청한 팀 목록 전부 삭제.
+  console.log(await db.getQueryResult('DELETE FROM Apply where RID = "'+getRID+'"'));
   res.json({success: true});
 }));
 
